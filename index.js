938f8fc62d0c53c1e84a8a6b48fb023b0c4e9923
Jonasaa00 <jonasaarhaug@gmail.com>
2022-01-08 15:40:00 +0100

    done

---
+++ index.js
@@ -0,0 +1,59 @@
+//first javascript code
+
+//task 1
+
+console.log ("hello world!");
+
+var pet = "dog";
+
+console.log (pet);
+
+ var JonasApples = 5;
+ var HeshApples = 10;
+
+ console.log (JonasApples + HeshApples); 
+ console.log (JonasApples < HeshApples)
+ 
+ //task 1^
+
+ //task 2
+
+ var firstName = "Jonas"
+ var lastName = "Aarhaug"
+ var car = "opel"
+
+console.log (firstName + " " + lastName + " " + car);
+
+ //task 2^
+
+ //task 3
+
+ var frog = "pet"
+ var dog = "pet"
+
+ console.log (frog + " " + dog);
+
+ //task 3^
+
+ //task 4
+ theOrderHasNotShipped= 5;
+ theOrderHasShipped= 6;
+
+console.log (theOrderHasShipped > theOrderHasNotShipped)
+console.log ("the order has shipped");
+
+ //task4^
+
+//task 5 
+ for (var i = 7; i <= 13; i++){
+ 
+ console.log (i);
+}
+
+//task 5^
+
+
+
+
+
+
